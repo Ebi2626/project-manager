8d28730221d1bff98279a9204bd55d5d4014f7eb
Edwin Harmata <edwin.harmata@gmail.com>
2019-12-16 18:11:34 +0100

    Set up, a project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import KanbanBoard from './KanbanBoard';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let cardsList = [
+  {
+    id: 1,
+    title: "Przeczytać ksiązkę",
+    description: "Całą książkę!",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Napisać trochę kodu",
+    description: "Będę przepisywać kod przykładów z książki",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "Przykład listy kontaktów",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Przykład Kanban",
+        done: false
+      },
+      {
+        id: 3,
+        name: "Moje eksperymenty",
+        done: false
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Zacząć projekt z tablicami Kanban",
+    description: "Jedynie zacząć :)",
+    status: "done",
+    tasks: []
+  }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<KanbanBoard cards ={cardsList} />, document.querySelector("#root"));
