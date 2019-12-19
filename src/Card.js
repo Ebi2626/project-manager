cf71a9af4874c13d74368aca9881d16c97c65cb5
Edwin Harmata <edwin.harmata@gmail.com>
2019-12-19 21:52:17 +0100

    adding some functionality to boards

--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
 
 class Card extends Component {
+    constructor(){
+        super();
+        this.state = {
+            cardDetails: false
+        }
+        this.showDetails = this.showDetails.bind(this);
+    }
+    showDetails(){
+        this.setState({
+                        cardDetails: !this.state.cardDetails})
+    }
     render(){
+        let cardDetails;
+        if(this.state.cardDetails){
+        cardDetails = <div className="card__details">
+        {this.props.description}
+        <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+        {}
+    </div>}
+
         return (
             <div className="card">
-                <div className="card__title">{this.props.title}</div>
-                <div className="card__details">
-                    {this.props.description}
-                    <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-                </div>
+    <div className={this.state.cardDetails?"card__title card__title--is-open":"card__title"} onClick={this.showDetails}>{this.props.title}</div>
+                {cardDetails}
             </div>
         )
     }
